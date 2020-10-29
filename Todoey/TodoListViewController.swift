f70304cd9bce53cb4b1cb4bf97f9a9d92b3c05de
Amber Yin <yin.ge0710@gmail.com>
2020-10-29 12:19:46 -0700

    linked up with TodoListViewController

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
